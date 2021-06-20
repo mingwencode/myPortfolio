1ba08d1ab24f5eabc3e0bb46c38565869014b683
mingwencode <mingwen314@gmail.com>
2021-06-19 22:45:05 -0700

    add photo modified type cursor

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
+const FEC = '../images/FEC_display.jpeg'
 
 const Portfolio = () => {
   return (
     <div className='single-page' id='portfolio'>
       <div className='portfolio-page'>
         <h1>Portfolio</h1>
+        <img src={FEC} width='300' />
       </div>
     </div>
-
-
 
   )
 }
