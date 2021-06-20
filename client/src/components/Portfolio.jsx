01ac0a6622add822b1382d6f2154fab7b187e283
mingwencode <mingwen314@gmail.com>
2021-06-19 23:16:55 -0700

    change photo size

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-const FEC = '../images/FEC_display.jpeg'
 
 const Portfolio = () => {
   return (
     <div className='single-page' id='portfolio'>
       <div className='portfolio-page'>
         <h1>Portfolio</h1>
-        <img src={FEC} width='300' />
+        <img src={'../images/FEC_display.jpeg'} width='300' />
+        <img src={'../images/sdc_display_sm.jpg'} width='300' />
+        <img src={'../images/smove_display_sm.jpg'} width='300' />
+        <img src={'../images/handy_display_sm.jpg'} width='300' />
       </div>
     </div>
 
