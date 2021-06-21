ce5c02740e1edc5768d177ba13b0c5ce4ff1468e
mingwencode <mingwen314@gmail.com>
2021-06-20 22:36:24 -0700

    change pics data format

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import Modal from './Modal.jsx'
+
+const displayPics = [
+  {
+    name: 'Triceratops Republic',
+    disPic: '../images/FEC_display.jpeg'
+  },
+  {
+    name: 'Project Catwalk System Design',
+    disPic: '../images/sdc_display_sm.jpg'
+  },
+  {
+    name: 'sMove',
+    disPic: '../images/smove_display_sm.jpg'
+  },
+  {
+    name: 'handyHelpers',
+    disPic: '../images/handy_display_sm.jpg'
+  },
+  {
+    name: 'Virus Shooter',
+    disPic: '../images/shooter_display_sm.jpg'
+  }
+]
 
 const Portfolio = () => {
+
+  const showModal = (e, job) => {
+    setModal(true);
+    setCurrentProject(pic)
+  }
+
   return (
     <div className='single-page' id='portfolio'>
       <div className='portfolio-page'>
         <h1>Portfolio</h1>
-        <img src={'../images/FEC_display.jpeg'} width='300' />
-        <img src={'../images/sdc_display_sm.jpg'} width='300' />
-        <img src={'../images/smove_display_sm.jpg'} width='300' />
-        <img src={'../images/handy_display_sm.jpg'} width='300' />
-        <img src={'../images/shooter_display_sm.jpg'} width='300' />
+        {displayPics.map((pic,index) => {
+          return <img src={pic.disPic} key={index} onClick={e => showModal(e, pic)} width='300' />
+        })}
       </div>
     </div>
 
