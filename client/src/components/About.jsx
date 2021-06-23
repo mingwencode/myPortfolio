95ae7b21b8485be21fa290d609f63c7320146cc6
mingwencode <mingwen314@gmail.com>
2021-06-22 22:29:50 -0700

    update font

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -12,17 +12,14 @@ const About = () => {
         </Row>
         <Row>
           <Col>
-            <img src={'../images/profile_pic.jpeg'} width="100" />
+            <img src={'../images/profile_pic.jpeg'} width="400" />
           </Col>
           <Col>
-            <p>Hi, I am Ming, a Software Engineer / Graphic Designer / Business Consultant. You probably wonder why this person worked in so many different roles? The truth is, they are all connected, their works are representing different forms of arts.
-
-            Software Engineers use a million lines of code to draw the human future. Designers express their feelings through colors. Business professionals see things no one else sees and bring them to life.
-
-            I am here to paint the future.
-
-            As a Software Engineer, I am currently working with:
-            Javascript | React | Node | Express | PostgreSQL | MongoDB | MySQL | Jest | React Testing Library | GraphQL | Docker | AWS | NGINX | Python</p>
+            <div>Hi, I am Ming, a Software Engineer / Graphic Designer / Business Consultant. You probably wonder why this person worked in so many different roles? The truth is, they are all connected, their works are representing different forms of arts.
+            </div><br />
+            <div>Software Engineers use a million lines of code to draw the human future. Designers express their feelings through colors. Business professionals see things no one else sees and bring them to life.</div><br />
+            <div>I am here to paint the future.</div><br />
+            <div>Javascript | React | Node | Express | PostgreSQL | MongoDB | MySQL | Jest | React Testing Library | GraphQL | Docker | AWS | NGINX | Python</div>
           </Col>
         </Row>
 
