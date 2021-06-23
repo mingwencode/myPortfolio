7700ac3e5bf2c09a0df47b59fd8a68db6881a721
mingwencode <mingwen314@gmail.com>
2021-06-23 00:18:56 -0700

    update color

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,8 +4,11 @@ const Contact = () => {
   return (
     <div className='single-page' id='contact'>
       <div className='contact-page'>
-        <h1>Get In Touch</h1>
-        <a className='media-link' href='mailto:mingwen314@gmail.com' rel='noopener noreferrer' target='_blank'>Say Hello</a><br></br>
+        <h1 className='page-title'>Get In Touch</h1>
+        <div>
+          I am available for hire. If you want to talk to me about an opportunity, a project collaboration or just say Hi, I will get back to to ASAP.
+        </div>
+        <a className='media-link' href='mailto:mingwen314@gmail.com' rel='noopener noreferrer' target='_blank'>SEND MESSAGE</a><br></br>
         <a className='media-link' href='https://github.com/mingwencode' rel='noopener noreferrer' target='_blank'>GitHub</a><br></br>
         <a className='media-link' href='https://www.linkedin.com/in/yimingwen/' rel='noopener noreferrer' target='_blank'>LinkedIn</a>
       </div>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,6 +3,15 @@ import Typist from 'react-typist';
 import Particles from "react-tsparticles";
 import { Link } from 'react-scroll';
 import Flip from 'react-reveal/Flip';
+import styled from 'styled-components';
+
+const NameDisplay = styled.span`
+  color: #C06014;
+`;
+
+const Intro = styled.div`
+  padding-bottom: 20px;
+`;
 
 const Home = () => {
 
@@ -22,16 +31,19 @@ const Home = () => {
     <div className='single-page' id='home'>
 
       <div className='home-page'>
-        <Typist cursor={{ hideWhenDone: true, hideWhenDoneDelay: 0, }} onTypingDone={() => buttonState()}>
-          <span>Hello, I am Ming Wen</span>
-          <br />
-          <span>I am a Designer<Typist.Delay ms={500} />
-          <Typist.Backspace count={8} delay={500} />
-          <Typist.Delay ms={300} />
-          Software Engineer
-          <Typist.Delay ms={200} />
-          </span>
-        </Typist>
+        <Intro>
+          <Typist cursor={{ hideWhenDone: true, hideWhenDoneDelay: 0, }} onTypingDone={() => buttonState()}>
+            <span>Hello, I am </span>
+            <NameDisplay>Ming Wen</NameDisplay>
+            <br />
+            <span>I am a Designer<Typist.Delay ms={500} />
+            <Typist.Backspace count={8} delay={500} />
+            <Typist.Delay ms={300} />
+            Software Engineer
+            <Typist.Delay ms={200} />
+            </span>
+          </Typist>
+        </Intro>
         {showButton ? <Flip left><button className='btnHome'><Link activeClass='active' to='portfolio' spy={true} smooth={true} duration={250}>View my work</Link></button></Flip> : null}
       </div>
     </div>
