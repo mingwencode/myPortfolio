563838e0b4318a25a97fd8e064aa836cd2deb51d
mingwencode <mingwen314@gmail.com>
2021-06-24 15:49:42 -0700

    modified landing page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Typist from 'react-typist';
 import { Link } from 'react-scroll';
 import Flip from 'react-reveal/Flip';
 import styled from 'styled-components';
 import ParticlesBg from 'particles-bg';
+import BIRDS from 'vanta/dist/vanta.birds.min'
+import GLOBE from 'vanta/dist/vanta.globe.min'
 
 const NameDisplay = styled.span`
   color: #C06014;
@@ -22,6 +24,33 @@ const IntroContainer = styled.div`
 const Home = () => {
 
   const [showButton, setShowButton] = useState(false)
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const myRef = useRef(null)
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      // setVantaEffect(GLOBE({
+      //   el: myRef.current,
+      //   mouseControls: true,
+      //   touchControls: true,
+      //   gyroControls: false,
+      //   minHeight: 200.00,
+      //   minWidth: 200.00,
+      //   scale: 1.00,
+      //   scaleMobile: 1.00,
+      //   color: 0xc06014,
+      //   color2: 0x0,
+      //   backgroundColor: 0xf4f4f4
+      // }))
+      setVantaEffect(BIRDS({
+        el: myRef.current,
+        backgroundColor: 0xf4f4f4
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
 
   const handleClick = (e) => {
     console.log('clicked')
@@ -32,6 +61,7 @@ const Home = () => {
   }
 
   return (
+    <div ref={myRef}>
     <div className='single-page' id='home'>
       <IntroContainer>
         <Intro>
@@ -50,10 +80,11 @@ const Home = () => {
         {showButton ? <Link activeClass='active' to='portfolio' spy={true} smooth={true} duration={250} offset={1}><Flip left><button className='btnHome'>View my work</button></Flip></Link> : null}
       </IntroContainer>
 
-      <ParticlesBg color='#EDDBCD' num={8} type='circle' bg={true} />
+      {/* <ParticlesBg color='#EDDBCD' num={8} type='circle' bg={true} /> */}
       {/* <ParticlesBg color={['#EDDBCD','#CDCDCD']} num={13} type='circle' bg={true} /> */}
       {/* <ParticlesBg color='#C06014' type='cobweb' num={150} bg={true} /> */}
 
+    </div>
     </div>
   )
 }
