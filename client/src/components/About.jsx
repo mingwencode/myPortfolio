2b5ba466d3d27d31b6db6c4054d6fca5326f2001
mingwencode <mingwen314@gmail.com>
2021-06-26 00:38:18 -0700

    updated about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -16,17 +16,19 @@ const About = () => {
           </Col>
         </Row>
         <Row>
-          <Col>
+          <Col className='d-flex justify-content-center' md={5}>
             <ScrollAnimation animateIn='fadeIn'>
-              <img src={'../images/profile_pic.jpeg'} width="400" />
+              <img className='profile-pic' src={'../images/profile_pic.jpeg'} width="400" />
             </ScrollAnimation>
           </Col>
-          <Col xs={7}>
-            <div className='inner-text-about'>Hi, I am Ming, a Software Engineer / Graphic Designer / Business Consultant. You probably wonder why this person worked in so many different roles? The truth is, they are all connected, their works are representing different forms of arts.
-            </div><br />
-            <div className='inner-text-about'>Software Engineers use a million lines of code to draw the human future. Designers express their feelings through colors. Business professionals see things no one else sees and bring them to life.</div><br />
-            <div className='inner-text-about'>I am here to paint the future.</div><br />
-            <div>Javascript | React | Node | Express | PostgreSQL | MongoDB | MySQL | Jest | React Testing Library | GraphQL | Docker | AWS | NGINX | Python</div>
+          <Col className='d-flex justify-content-center' md={7}>
+            <div>
+              <p className='inner-text-about'>Hi, I am Ming, a Software Engineer / Graphic Designer / Business Consultant. You probably wonder why this person worked in so many different roles? The truth is, they are all connected, their works are representing different forms of arts.
+              </p><br />
+              <p className='inner-text-about'>Software Engineers use a million lines of code to draw the human future. Designers express their feelings through colors. Business professionals see things no one else sees and bring them to life.</p><br />
+              <p className='inner-text-about'>I am here to paint the future.</p><br />
+              <p>Javascript | React | Node | Express | PostgreSQL | MongoDB | MySQL | Jest | React Testing Library | GraphQL | Docker | AWS | NGINX | Python</p>
+            </div>
           </Col>
         </Row>
       </Container>
