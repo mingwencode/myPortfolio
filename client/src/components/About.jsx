24b5cd5531b9ca7ecae3f21817470a2cc7f94d36
mingwencode <mingwen314@gmail.com>
2021-06-27 13:12:46 -0700

    added resume

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -9,6 +9,13 @@ const ColorText = styled.p`
   color: #C06014;
 `;
 
+const ResumeBtn = styled.a`
+  &:hover {
+    text-decoration-line: none;
+  }
+
+`;
+
 // const About = () => {
 //   return (
 //     <div className='single-page' id='about'>
@@ -63,12 +70,12 @@ const About = () => {
             </Row>
             <Row>
               <Col className='d-flex justify-content-center'>
-            <div>
-              <p className='inner-text-about'>Hi, I am Ming, a Software Engineer / Graphic Designer / Business Consultant. You probably wonder why this person worked in so many different roles? The truth is, they are all connected, their works are representing different forms of arts.
-              </p><br />
-              <p className='inner-text-about'>Software Engineers use a million lines of code to draw the human future. Designers express their feelings through colors. Business professionals see things no one else sees and bring them to life.</p><br />
-              <ColorText className='inner-text-about'>I am here to paint the future.</ColorText><br />
-            </div>
+                <div>
+                  <p className='inner-text-about'>Hi, I am Ming, a Software Engineer / Graphic Designer / Business Consultant. You probably wonder why this person worked in so many different roles? The truth is, they are all connected, their works are representing different forms of arts.
+                  </p><br />
+                  <p className='inner-text-about'>Software Engineers use a million lines of code to draw the human future. Designers express their feelings through colors. Business professionals see things no one else sees and bring them to life.</p><br />
+                  <ColorText className='inner-text-about'>I am here to paint the future.</ColorText><br />
+                </div>
               </Col>
             </Row>
             <Row className='about-tech-text'>
@@ -81,6 +88,7 @@ const About = () => {
                 <li>PostgreSQL</li>
                 <li>MongoDB</li>
                 </ul>
+                <br />
               </Col>
               <Col>
                 <ul>
@@ -91,6 +99,12 @@ const About = () => {
                 <li>Niginx</li>
                 <li>Python</li>
               </ul>
+              <br />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <ResumeBtn className='btnHome' href='./Ming_Wen_Resume.pdf' rel='noopener noreferrer' target='_blank'>Resume</ResumeBtn>
               </Col>
             </Row>
           </Container>
